a26b3fa1c4f801768d8a55da892ba5162fe1c1ca
Dmitiriy Cherkashyn <dmitriy.cherkashyn@gmail.com>
2017-09-01 18:44:32 +0300

    rest of

--- app/Proxy.php
+++ app/Proxy.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Proxy extends Model {
+    protected $table = "proxies";
     protected $fillable = [
             'host', 'port', 'login',
             'password', 'country', 'status',
