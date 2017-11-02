c14d72a2ecd71de9b93ba29deac0e5698c8e08aa
Wigberht <dmitriy.cherkashyn@gmail.com>
2017-11-02 16:35:14 +0200

    font changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,6 @@
     <!-- Fonts -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
-
-
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
