6d8ce258d91dd07e9a9c3692adab23c3ff7c6604
Wigberht <dmitriy.cherkashyn@gmail.com>
2017-11-10 14:35:29 +0200

    history loading reworked/speed up

--- app/Services/Qiwi/QiwiReports.php
+++ app/Services/Qiwi/QiwiReports.php
@@ -41,38 +41,48 @@ trait QiwiReports {
 
         $html = $response->getBody()->getContents();
         $crawler = new Crawler($html);
-        $items = $crawler
-                ->filter('.reportsLine[data-container-name="item"]')
-                ->each(function (Crawler $node, $i) use ($start, $size) {
+        $items = $crawler->filter('.reportsLine[data-container-name="item"]');
+        $transactions = [];
+        //        for ($j = $start; $j < $start + $size; $j++) {
+        //            $node = $items[$j];
+        foreach ($items as $i => $node) {
+            //                ->each(function (Crawler $node, $i) use ($start, $size) {
+            //        $items = $crawler
+            //                ->filter('.reportsLine[data-container-name="item"]')
+            //                ->each(function (Crawler $node, $i) use ($start, $size) {
 
-                    // 'ignore' unnecessary nodes
-                    if ($i < $start || $i >= $size + $start) return null;
+            //            dd(['node' => $node]);
+            // 'ignore' unnecessary nodes
+            if ($i < $start) continue;
+            if ($i >= $size + $start) break;
+            //            if ($i < $start || $i >= $size + $start) return null;
 
-                    $transaction = new Transaction();
-                    $transaction->date = trim($node->filter('.date')->first()->text());
-                    $transaction->time = trim($node->filter('.time')->first()->text());
-                    $transaction->transaction = trim($node->filter('.transaction')->first()->text());
-                    $transaction->status = strtolower(str_replace('reportsLine status_', '', $node->attr('class')));
-                    $transaction->provider = trim($node->filter('.ProvWithComment .provider span')->first()->text());
-                    $transaction->opNumber = trim($node->filter('.ProvWithComment .provider .opNumber')->first()->text());
-                    $transaction->comment = trim($node->filter('.ProvWithComment .comment')->first()->text());
-                    $transaction->amount = trim($node->filter('.originalExpense span')->first()->text());
-                    $transaction->sign = $this->setAmountSign($node->filter('.IncomeWithExpend')->attr('class'));
-                    $transaction->commission = trim($node->filter('.commission')->first()->text());
+            $node = new Crawler($node);
+            $transaction = new Transaction();
+            $transaction->date = trim($node->filter('.date')->first()->text());
+            $transaction->time = trim($node->filter('.time')->first()->text());
+            $transaction->transaction = trim($node->filter('.transaction')->first()->text());
+            $transaction->status = strtolower(str_replace('reportsLine status_', '', $node->attr('class')));
+            $transaction->provider = trim($node->filter('.ProvWithComment .provider span')->first()->text());
+            $transaction->opNumber = trim($node->filter('.ProvWithComment .provider .opNumber')->first()->text());
+            $transaction->comment = trim($node->filter('.ProvWithComment .comment')->first()->text());
+            $transaction->amount = trim($node->filter('.originalExpense span')->first()->text());
+            $transaction->sign = $this->setAmountSign($node->filter('.IncomeWithExpend')->attr('class'));
+            $transaction->commission = trim($node->filter('.commission')->first()->text());
 
-                    if ($transaction->status == "error") {
-                        $transaction->errorMessage = json_decode($node->filter('.IncomeWithExpend .operations .error')->attr('data-params'))->message;
-                    }
+            if ($transaction->status == "error") {
+                $transaction->errorMessage = json_decode($node->filter('.IncomeWithExpend .operations .error')->attr('data-params'))->message;
+            }
 
-                    return $transaction;
-                });
+            $transactions[] = $transaction;
+        };//);
 
         //remove nulls
-        $items = array_filter($items, function ($item) {
-            return $item !== null;
-        });
+        //        $items = array_filter($items, function ($item) {
+        //            return $item !== null;
+        //        });
 
-        return $items;
+        return $transactions;
     }
 
     /**
